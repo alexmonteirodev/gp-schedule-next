f0179dfe32edf4cff22bb5ae78ea03635293807f
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-07-04 13:37:45 +0200

    build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { useCalendarContext } from "./Context";
-import HoursCount from "./HoursCount";
+// import HoursCount from "./HoursCount";
 const Header = () => {
   const { calendar, currentMonth, hours, totals } = useCalendarContext();
   console.log("hours", hours);
