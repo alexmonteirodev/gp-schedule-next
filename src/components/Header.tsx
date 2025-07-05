bc260accd33a3fc0ab573a220f5e9a8202f7116c
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-07-05 11:34:42 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,10 @@ const Header = () => {
           <h1 className="font-bold text-3xl">{monthNames[currentMonth - 1]}</h1>
         </div>
         <div className="text-base-400 font-medium">
-          Total: <span className="italic">{totals[currentMonth - 1]} hrs</span>
+          Total:{" "}
+          <span className="italic">
+            {totals ? `${totals[currentMonth - 1]} hrs` : "-"} hrs
+          </span>
         </div>
       </div>
     </div>
