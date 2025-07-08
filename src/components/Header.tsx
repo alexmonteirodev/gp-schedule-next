2dc753e562dd3cc3355aed547f180cbb5f7e6ed0
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-07-08 18:33:08 +0200

    update days worked feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import settings from "../../public/settings.svg";
 import { useRouter } from "next/navigation";
 const Header = () => {
   const [settingsOn, setSettingsOn] = React.useState(false);
-  const { calendar, currentMonth, totals } = useCalendarContext();
+  const { calendar, currentMonth, totals, hours } = useCalendarContext();
   const router = useRouter();
 
   if (!currentMonth) return;
@@ -28,33 +28,8 @@ const Header = () => {
 
   return (
     <div className="mt-5 mb-5">
-      {/* <div className="flex">
-        <h4 className="font-bold m-auto mb-5">SPINUP</h4>
-        <Image className={`h-5 w-5`} src={settings} alt="settings.svg" />
-      </div> */}
-      <div className="flex items-center justify-between">
-        <div className="flex">
-          <div className="pr-5 border-r-2 border-base-200">
-            <p className="text-base-400 text-[0.8rem]">
-              {calendar ? calendar.year : "Year"}
-            </p>
-            <h1 className="font-bold text-3xl">
-              {monthNames[currentMonth - 1]}
-            </h1>
-          </div>
-          <div className="pl-5">
-            <div>
-              <p className="text-base-400">Worked this month</p>
-            </div>
-            <div className="text-base-800 font-medium flex text-2xl space-x-2">
-              <span className="">0 days</span>
-              <span>|</span>
-              <span className="">
-                {totals ? `${totals[currentMonth - 1]}` : "-"}hrs
-              </span>
-            </div>
-          </div>
-        </div>
+      <div className="flex">
+        <h4 className="font-bold m-auto mb-5">SpinUp</h4>
         <button
           onTouchStart={() => {
             setSettingsOn(!settingsOn);
@@ -68,6 +43,43 @@ const Header = () => {
         >
           <Image src={settings} alt="x" className="h-5 w-5" priority />
         </button>
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="flex">
+          <div className="pr-5 border-r-2 border-base-200">
+            <p className="text-base-400 text-[0.8rem]">
+              {calendar ? calendar.year : "Year"}
+            </p>
+            <h1 className="font-bold text-[1.675rem]">
+              {monthNames[currentMonth - 1]}
+            </h1>
+          </div>
+          <div className="pl-5">
+            <div>
+              <p className="text-base-400 text-[0.8rem]">Worked this month</p>
+            </div>
+            <div className="text-base-800 font-medium flex text-2xl space-x-2">
+              <span className="">{hours?.[currentMonth - 1].length}days</span>
+              <span>|</span>
+              <span className="">
+                {totals ? `${totals[currentMonth - 1]}` : "-"}hrs
+              </span>
+            </div>
+          </div>
+        </div>
+        {/* <button
+          onTouchStart={() => {
+            setSettingsOn(!settingsOn);
+            setTimeout(() => {
+              router.push("/settings");
+            }, 300);
+          }}
+          className={`self-start rounded-full transition-transform duration-300 pointer-events-auto ${
+            settingsOn ? "rotate-135" : "rotate-0"
+          }`}
+        >
+          <Image src={settings} alt="x" className="h-5 w-5" priority />
+        </button> */}
       </div>
     </div>
   );
